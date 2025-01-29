1fff9aeacd6fde1f8e65e9865c09a7ad7fc4c208
Jasper Burvill <jasperburvill@gmail.com>
2025-01-29 09:30:17 +0000

    Fixing input validation

--- Prelecture1/input_validation.cpp
+++ Prelecture1/input_validation.cpp
@@ -1,20 +1,19 @@
 // PL1/input_validation.cpp
 // Example: checking for valid input (PHYS30762)
 // Niels Walet, last updated by Charanjit Kaur, 23/01/24
-// test comment
 #include<iostream>
 int main() 
 {
   int any_year; 
   std::cout<<"Enter a year: "; 
-  std::cin>>any_year;
+  std::cin>>any_year; // Takes an integer
   // Check input is valid 
-  while(std::cin.fail()) 
+  while(std::cin.fail()) // Fail is true if cin is not an integer
   {
     std::cout<<"Sorry, your input was not valid, please enter a year: "; 
     // Clear fail bit and ignore bad input
     std::cin.clear(); 
-    std::cin.ignore(); 
+    std::cin.ignore('\n'); // Adding '\n' skips to the next new line
     std::cin>>any_year;
   }
   std::cout<<"C++ is the best programming language in "<<any_year<<"!"<<std::endl;
